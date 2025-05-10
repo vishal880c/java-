542101c4a8f954d37991105317736bbe19bca3f5
vishal880c <vishalkr3r@gmail.com>
2025-05-10 12:43:05 +0530

    Add files via upload

---
+++ 4 Swap.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+ class Swap {
+    public static void main(String[] args) {
+        Scanner input  =  new Scanner(System.in);
+        System.out.println(" welcome to Swaping station \n\n ");
+        System.out.println(" Enter value of A: ");
+        int a = input.nextInt();
+        System.out.println("Enter the value B:");
+        int b = input.nextInt();
+
+
+        int c =a;
+        a = b;
+        b = c;
+
+        System.out.println(" Swapping done...");
+        System.out.println( " Value of A is: " +  a);
+        System.out.println(" Value of B is " + b  );
+
+
+
+
+
+    }
+}
